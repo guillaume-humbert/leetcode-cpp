3c5a668c251027f53a9f221da84a0b970c68f712
Guillaume J Humbert <guillaume.j.humbert@pm.me>
2021-04-23 06:33:28 +0800

    Fix two sum

--- 1_two_sum/ts.cc
+++ 1_two_sum/ts.cc
@@ -3,20 +3,6 @@
 
 std::vector<int> twoSum(const std::vector<int> &nums, int target)
 {
-    std::unordered_map<int, int> cache;
     std::vector<int> res;
-
-    for (int i = 0; i < nums.size(); ++i)
-    {
-        auto found = cache.find(target - nums[i]);
-        if (found != cache.end())
-        {
-            res.push_back(cache[target - nums[i]]);
-            res.push_back(i);
-            return res;
-        }
-
-        cache[nums[i]] = i;
-    }
     return res;
 }
